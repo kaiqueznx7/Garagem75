f7f38cfbfe94d4e0059c2c4b376af1a3e4234e5f
Beto Barba <betosilvc@hotmail.com>
2025-09-24 20:06:52 -0300

    interfaces

--- Garagem75/Interfaces/IClienteRepository.cs
+++ Garagem75/Interfaces/IClienteRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Garagem75.Interfaces
+﻿using Garagem75.Models;
+
+namespace Garagem75.Interfaces
 {
     public interface IClienteRepository
     {
+        Task<List<Cliente>> GetAllAsync();
+
+        //stand by
+        Task<Cliente> GetByIdAsync(int id);
+        Task AddAsync(Cliente cliente);
+        Task UpdateAsync(Cliente cliente);
+        Task DeleteAsync(int id);
     }
 }
--- Garagem75/Interfaces/IEnderecoRepository.cs
+++ Garagem75/Interfaces/IEnderecoRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Garagem75.Interfaces
+﻿using Garagem75.Models;
+
+namespace Garagem75.Interfaces
 {
     public interface IEnderecoRepository
     {
+        Task<List<Endereco>> GetAllAsync();
+
+        //stand by
+        Task<Endereco> GetByIdAsync(int id);
+        Task AddAsync(Endereco endereco);
+        Task UpdateAsync(Endereco endereco);
+        Task DeleteAsync(int id);
     }
 }
