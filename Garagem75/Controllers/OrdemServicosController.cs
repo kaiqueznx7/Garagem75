23dd2d46a197ccebefc01bcdcbab199311257e9d
Kaique Castro <kaiquefcastro.06@gmail.com>
2025-09-24 21:01:30 -0300

    Controllers

---
+++ Garagem75/Controllers/OrdemServicosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garagem75.Data;
+using Garagem75.Models;
+
+namespace Garagem75.Controllers
+{
+    public class OrdemServicosController : Controller
+    {
+        private readonly Garagem75DBContext _context;
+
+        public OrdemServicosController(Garagem75DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrdemServicos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.OrdemServicos.ToListAsync());
+        }
+
+        // GET: OrdemServicos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ordemServico = await _context.OrdemServicos
+                .FirstOrDefaultAsync(m => m.IdOrdemServico == id);
+            if (ordemServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordemServico);
+        }
+
+        // GET: OrdemServicos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrdemServicos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdOrdemServico,Descricao,DataServico,MaoDeObra,ValorDesconto,ValorTotal,Status,DataEntrega")] OrdemServico ordemServico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ordemServico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ordemServico);
+        }
+
+        // GET: OrdemServicos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ordemServico = await _context.OrdemServicos.FindAsync(id);
+            if (ordemServico == null)
+            {
+                return NotFound();
+            }
+            return View(ordemServico);
+        }
+
+        // POST: OrdemServicos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdOrdemServico,Descricao,DataServico,MaoDeObra,ValorDesconto,ValorTotal,Status,DataEntrega")] OrdemServico ordemServico)
+        {
+            if (id != ordemServico.IdOrdemServico)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ordemServico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrdemServicoExists(ordemServico.IdOrdemServico))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ordemServico);
+        }
+
+        // GET: OrdemServicos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ordemServico = await _context.OrdemServicos
+                .FirstOrDefaultAsync(m => m.IdOrdemServico == id);
+            if (ordemServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordemServico);
+        }
+
+        // POST: OrdemServicos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ordemServico = await _context.OrdemServicos.FindAsync(id);
+            if (ordemServico != null)
+            {
+                _context.OrdemServicos.Remove(ordemServico);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrdemServicoExists(int id)
+        {
+            return _context.OrdemServicos.Any(e => e.IdOrdemServico == id);
+        }
+    }
+}
---
+++ Garagem75/Controllers/VeiculoController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garagem75.Data;
+using Garagem75.Models;
+
+namespace Garagem75.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        private readonly Garagem75DBContext _context;
+
+        public VeiculoController(Garagem75DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Veiculo
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Veiculos.ToListAsync());
+        }
+
+        // GET: Veiculo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos
+                .FirstOrDefaultAsync(m => m.IdVeiculo == id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Veiculo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdVeiculo,Modelo,Ano,Placa,Cor,Fabricante")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+            return View(veiculo);
+        }
+
+        // POST: Veiculo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdVeiculo,Modelo,Ano,Placa,Cor,Fabricante")] Veiculo veiculo)
+        {
+            if (id != veiculo.IdVeiculo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(veiculo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VeiculoExists(veiculo.IdVeiculo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos
+                .FirstOrDefaultAsync(m => m.IdVeiculo == id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(veiculo);
+        }
+
+        // POST: Veiculo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo != null)
+            {
+                _context.Veiculos.Remove(veiculo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VeiculoExists(int id)
+        {
+            return _context.Veiculos.Any(e => e.IdVeiculo == id);
+        }
+    }
+}
