33f14d8ed40db332dca0b74eaa1bbc9ac66c277c
Kaique Castro <kaiquefcastro.06@gmail.com>
2025-10-07 20:23:44 -0300

    Botton Inicio

---
+++ Garagem75/Migrations/20251007231309_ano1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garagem75.Migrations
+{
+    /// <inheritdoc />
+    public partial class ano1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
