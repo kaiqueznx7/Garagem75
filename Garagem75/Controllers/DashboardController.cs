5813d462a6cd8f7173ed5c1b1f952a52df0122ca
Kaique Castro <kaiquefcastro.06@gmail.com>
2025-10-13 22:03:44 -0300

    Atualização de Dash

--- Garagem75/Controllers/DashboardController.cs
+++ Garagem75/Controllers/DashboardController.cs
@@ -24,19 +24,7 @@ namespace Garagem75.Controllers
             vm.TotalOrdensServico = await _context.OrdemServicos.CountAsync();
             vm.TotalUsuarios = await _context.Usuarios.CountAsync();
 
-            //Ultimas Peças Cadastradas
-            vm.UltimasPecas = await _context.Pecas
-                .AsNoTracking()
-                .OrderByDescending(p => p.IdPeca)               
-                .Select(p => new PecaItem
-                {
-                    IdPeca = p.IdPeca,
-                    Marca = p.Marca,
-                    Nome = p.Nome,
-                    Preco = p.Preco
-                })
-                .Take(4)
-                .ToListAsync();
+        
 
             //Clientes Mais Antigos
             vm.ClientesMaisAntigos = await _context.Clientes
@@ -54,7 +42,20 @@ namespace Garagem75.Controllers
                 .ToListAsync();
 
             //Marcas de Peças Mais Usadas
-            
+            vm.UltimosVeiculosAtendidos = await _context.Veiculos
+                .AsNoTracking()
+                .OrderByDescending(v => v.IdVeiculo)
+                .Select(v => new ModeloVeiculos
+                {
+                    IdVeiculo = v.IdVeiculo,
+                    Fabricante = v.Fabricante,
+                    Modelo = v.Modelo,
+                    Placa = v.Placa
+                })
+                .Take(5)
+                .ToListAsync();
+
+
             return View(vm);
         }     
     }
